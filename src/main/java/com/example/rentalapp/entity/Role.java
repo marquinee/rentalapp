0ccdeec1d81c109866c8456febdea0bee424793c
Marina <marinall11xx@gmail.com>
2025-11-06 14:32:43 +0300

    [UPD] roles tables added

---
+++ src/main/java/com/example/rentalapp/entity/Role.java
@@ -0,0 +1,19 @@
+package com.example.rentalapp.entity;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+}
---
+++ src/main/java/com/example/rentalapp/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.rentalapp.entity;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+}
